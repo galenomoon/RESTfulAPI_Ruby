7fe1ed005c224343c22ed1330052ccd651b4ffa7
galeno <gui.galenocover@gmail.com>
2022-03-15 19:35:49 -0300

    serializer ajusts

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,5 +1,15 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
+
+  #Associations
+  belongs_to :kind #, optional: true
+  has_many :phones
+  has_one :address
+
+  meta do
+    {author: "Galeno"}
+  end
+
   def attributes(*args)
     h = super(*args)
     #h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
