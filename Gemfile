cb4a49265ed12496349f5e1016d146312645c2d3
galeno <gui.galenocover@gmail.com>
2022-03-16 15:05:34 -0300

    Version Controller

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem "sqlite3"
 gem "puma", "~> 3.0"
 gem 'jwt'
 gem 'foreman'
+gem 'versionist'
+# gem 'devise_token_auth'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,42 @@
 Rails.application.routes.draw do
   resources :auths, only: [:create]
   resources :kinds
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
 
-    #Singular Phone
-    resource :phone, except: [:show]
-    resource :phone, except: [:show], path: 'relationships/phones'
+  scope module: 'v1' do
+    resources :contacts, :constraints => lambda {|request|  request.params[:version] == "1" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
 
-    #Plural Phone
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
+      #Singular Phone
+      resource :phone, except: [:show]
+      resource :phone, except: [:show], path: 'relationships/phones'
 
-    resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      #Plural Phone
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
   end
 
+  scope module: 'v2' do
+    resources :contacts, :constraints => lambda {|request|  request.params[:version] == "2" }  do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      #Singular Phone
+      resource :phone, except: [:show]
+      resource :phone, except: [:show], path: 'relationships/phones'
+
+      #Plural Phone
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  end
   #RESOURCES | Cria um padrão REST de rotas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
